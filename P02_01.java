753fa803ee9e4e1c2a0963f266e63a4fb0b1fe1c
graciapardede <graciapardede30@gmail.com>
2023-09-30 20:51:29 +0700

    Saya telah mengerjaka tugas yang telah diberikan

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,47 @@
-// NIM - Your Name
+// 12S23044 - Gracia Pardede
+import java.util.*;
+import java.lang.Math;
+
+public class P02_01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String deskripsiTugas;
+        String kodeMataKuliah;
+        String namaMataKuliah;
+        String pengampu;
+        String deadline;
+        String formatPenamaanTugas;
+        String status;
+        double tingkatKesulitan, hariHinggaDeadline, prioritas;
+
+        deskripsiTugas = input.nextLine();
+        kodeMataKuliah = input.nextLine();
+        namaMataKuliah = input.nextLine();
+        pengampu = input.nextLine();
+        deadline = input.nextLine();
+        formatPenamaanTugas = input.nextLine();
+        tingkatKesulitan = Double.parseDouble(input.nextLine());
+        hariHinggaDeadline = Double.parseDouble(input.nextLine());
+        prioritas = Double.parseDouble(input.nextLine());
+        status = input.nextLine();
+        prioritas = tingkatKesulitan * (1.0 / hariHinggaDeadline);
+        if (prioritas > 3) {
+            status = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas >= 1.5 && prioritas <= 3) {
+                status = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    status = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println("Prioritas: " + toFixed(prioritas,2));
+        System.out.println(deskripsiTugas + "|" + kodeMataKuliah + "|" + namaMataKuliah + "|" + pengampu + "|" + deadline + "|" + formatPenamaanTugas);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,41 @@
-//NIM -Nama
+//12S23044-Gracia Pardede
+import java.util.*;
+import java.lang.Math;
+
+public class P02_02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namaGudang;
+        int kapasitasGudang;
+        int jumlahBuku;
+        String aC;
+        String lantai;
+        String ketersediaanTeknologi;
+        int stok;
+        String penilaian;
+        int skor;
+
+        namaGudang = input.nextLine();
+        kapasitasGudang = Integer.parseInt(input.nextLine());
+        jumlahBuku = Integer.parseInt(input.nextLine());
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanTeknologi = input.nextLine();
+        stok = Integer.parseInt(input.nextLine());
+        skor = Integer.parseInt(input.nextLine());
+        if (skor >= 40) {
+            penilaian = "Gudang Elite";
+        } else {
+            if (skor >= 25 && skor <= 40) {
+                penilaian = "Gudang Standar";
+            } else {
+                if (skor <= 25) {
+                    penilaian = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println("Informasi Gudang: ");
+        System.out.println(namaGudang + "|" + kapasitasGudang + "|" + jumlahBuku + "|" + aC + "|" + lantai + "|" + ketersediaanTeknologi + "|" + penilaian);
+    }
+}
